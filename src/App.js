6144523945379bfb73792e6bb5c5084ea80e4437
Jonathan Townend <jonathan.townend@gmail.com>
2021-06-17 22:35:42 +0100

    Add instructions, clear button, and some UI animations and states

--- src/App.js
+++ src/App.js
@@ -33,27 +33,47 @@ const App = () => {
     setEntries(newEntries)
   }
 
+  const handleClear = () => {
+    const newEntries = entries.map(entry => {
+      return {
+        ...entry,
+        isStarred: false,
+        isChecked: false,
+      }
+    })
+    setEntries(newEntries)
+  }
+
   return (
     <div className="App">
-      <h1>🙌 Weekend Bingo! 🙌</h1>
-      <div className="entries-container">
-        {entries.map(entry => (
-          <Entry
-            title={entry.title}
-            checked={entry.isChecked}
-            starred={entry.isStarred}
-            onChecked={() => handleCheckboxChange(entry.id)}
-            onStarred={() => handleStarChange(entry.id)}
-          />
-        ))}
+      <div className="bingo-board">
+        <h1>🙌 Weekend Bingo! 🙌</h1>
+        <div className="entries-container">
+          {entries.map(entry => (
+            <Entry
+              title={entry.title}
+              checked={entry.isChecked}
+              starred={entry.isStarred}
+              onChecked={() => handleCheckboxChange(entry.id)}
+              onStarred={() => handleStarChange(entry.id)}
+            />
+          ))}
+        </div>
+        <Counter
+          count={entries.map(entry => entry.isChecked ? 1 : 0).reduce((acc, count) => acc + count, 0)}
+          total={entries.length}
+        />
+        {entries.find(entry=> entry.isStarred && entry.isChecked) && (
+          <h4>Your starred topic has been mentioned! 🎉</h4>
+        )}
+        <div className="clear-button-container">
+          <div className="clear-button" onClick={handleClear}>Clear</div>
+        </div>
       </div>
-      <Counter
-        count={entries.map(entry => entry.isChecked ? 1 : 0).reduce((acc, count) => acc + count, 0)}
-        total={entries.length}
-      />
-      {entries.find(entry=> entry.isStarred && entry.isChecked) && (
-        <h4>Your starred topic has been mentioned! 🎉</h4>
-      )}
+      <div className="instructions">
+        <p>✅ Use the checkboxes to keep track of activities mentioned in weekly catchups.</p>
+        <p>⭐ Star an activitiy that you think is likely to come up. Extra points if you get a starred item!</p>
+      </div>
     </div>
   );
 }
--- src/components/counter/counter.component.jsx
+++ src/components/counter/counter.component.jsx
@@ -1,10 +1,11 @@
 import './counter.styles.css'
 
 export const Counter = ({count, total}) => {
+  const scale = 1 + count/total
   return (
-    <div>
+    <div style={{transform: `scale(${scale})`}}>
       {count !== total && <h3>{`${count} out of ${total}`}</h3>}
-      {count === total && <h3>Full house! 🥳</h3>}
+      {count === total && <h3>🎉 Full house! 🎉</h3>}
     </div>
   )
 }
